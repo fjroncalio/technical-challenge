d318a1c105d2e324624c4e7a605b27b3cc3781f8
Fabricio Roncalio <fabricio.roncalio@5by5.com.br>
2021-06-17 20:29:44 -0300

    adding mediator

---
+++ src/TechnicalChallenge.Api/TechnicalChallenge.Domain.Core/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TechnicalChallenge.Domain.Core
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection AddDomainContext(this IServiceCollection services)
+        {
+            services.AddMediatR(AppDomain.CurrentDomain.Load("TechnicalChallenge.Domain"));
+
+            return services;
+        }
+    }
+}
