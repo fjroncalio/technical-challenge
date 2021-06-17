03421d88ab144ad3ec2c39a1c6af70a655c62afe
Fabricio Roncalio <fabricio.roncalio@5by5.com.br>
2021-06-17 20:29:44 -0300

    adding mediator

--- src/TechnicalChallenge.Api/TechnicalChallenge.Api/Startup.cs
+++ src/TechnicalChallenge.Api/TechnicalChallenge.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using TechnicalChallenge.Domain.Core;
 
 namespace TechnicalChallenge.Api
 {
@@ -15,17 +16,17 @@ namespace TechnicalChallenge.Api
         }
 
         public IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDomainContext();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechnicalChallenge.Api", Version = "v1" });
             });
         }
-        
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +39,7 @@ namespace TechnicalChallenge.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
